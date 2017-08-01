dc3e16688d521f2a1b9203144383aac5be2987c7
Delicay <>
2017-08-01 09:32:41 +0800

    just try it!

---
+++ src/Testmyself/TestMyself.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Testmyself;
+
+/**
+ *
+ * @author Aoyue
+ */
+public class TestMyself {
+public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Hello,Delicay");
+    }
+    
+}
